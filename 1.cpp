6bedc59a3f47fb3be4284ef3baf3bb1460a18fd7
riya19nanditaverma-hub <riya19nanditaverma@gmail.com>
2026-06-25 02:41:06 +0530

    Added Patterns_basicmethod

--- 1.cpp
+++ 1.cpp
@@ -1,11 +1,7 @@
 #include <iostream>
 using namespace std;
-
 int main(){
-    cout<<"HELLO WORLD.";
+    cout<<"****\n***\n**\n*\n";
     return 0;
 }
 
-//writting ls in the terminal after the name of the folder will show all the files in that folder.
-//g++ 1.cpp -o 1.exe (to compile the code and create an executable file named 1.exe)
-//./1.exe (to run the executable file and see the output in the terminal). 
